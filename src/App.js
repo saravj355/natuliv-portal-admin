30591f314f724a87e9ce9f8efa8407648e382bd3
saravj355 <svj971@gmail.com>
2021-04-09 11:16:50 -0500

    add main dashboard and dataProvider

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import React from 'react'
+import simpleRestProvider from 'ra-data-simple-rest';
+import React from 'react';
+import { Admin, Resource } from 'react-admin';
+import Dashboard from './components/Dashboard';
 
-function App() {
+function App () {
     return (
-        <div className="App">
-            Hola
-        </div>
-    )
+        <Admin dashboard={Dashboard} dataProvider={simpleRestProvider('http://localhost:5000')}>
+            <Resource name="buyers/search"/>
+        </Admin>
+    );
 }
 export default App;
