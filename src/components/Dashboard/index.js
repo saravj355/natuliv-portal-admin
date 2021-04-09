59e05d3f970f5459cc8b93ef171dbede0fdeaf6d
saravj355 <svj971@gmail.com>
2021-04-09 11:17:20 -0500

    add main dashboard

---
+++ src/components/Dashboard/index.js
@@ -0,0 +1,11 @@
+import { Card, CardContent, CardHeader } from '@material-ui/core';
+import * as React from 'react';
+
+const Dashboard = () => (
+    <Card>
+        <CardHeader title="Welcome to the administration" />
+        <CardContent>Welcome to Natuliv</CardContent>
+    </Card>
+);
+
+export default Dashboard;
