72c313528476159ac402fd517618a2882e8d3521
saravj355 <svj971@gmail.com>
2021-04-12 11:26:49 -0500

    add handle queries

---
+++ src/data-provider/Query.js
@@ -0,0 +1,20 @@
+export default function handleQueryString (params) {
+    const { page, perPage } = params.pagination;
+    const { field, order } = params.sort;
+    const queryString = { query: '' };
+
+    if (params.target) {
+        queryString.query += `${params.target}=${params.id}`;
+    }
+
+    if (params.filter) {
+        // sort field
+        queryString.query += `&sortBy=${field}`;
+        queryString.query += `&sortOrder=${order}`;
+        // range field
+        queryString.query += `&offset=${(page - 1) * perPage}`;
+        queryString.query += `&limit=${page * perPage - 1}`;
+    }
+
+    return queryString.query;
+}
