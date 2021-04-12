b0d7d9b2b5bcf42b1b89bb4af55f23bd09db4370
saravj355 <svj971@gmail.com>
2021-04-12 11:24:56 -0500

    add admin view resources

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-import simpleRestProvider from 'ra-data-simple-rest';
+import { Group, LocalMall, StoreMallDirectory } from '@material-ui/icons';
 import React from 'react';
 import { Admin, Resource } from 'react-admin';
+import Buyer from './components/Buyers';
 import Dashboard from './components/Dashboard';
+import Product from './components/Products';
+import Vendor from './components/Vendors';
+import DataProvider from './data-provider';
 
 function App () {
     return (
-        <Admin dashboard={Dashboard} dataProvider={simpleRestProvider('http://localhost:5000')}>
-            <Resource name="buyers/search"/>
+        <Admin dashboard={Dashboard} dataProvider={DataProvider}>
+            <Resource name="buyers" icon={Group}list={Buyer.List} edit={Buyer.Edit} create={Buyer.Create}/>
+            <Resource name="products" icon={LocalMall}list={Product.List} edit={Product.Edit} create={Product.Create}/>
+            <Resource name="vendors" icon={StoreMallDirectory}list={Vendor.List} edit={Vendor.Edit} show={Vendor.Show} create={Vendor.Create}/>
         </Admin>
     );
 }
