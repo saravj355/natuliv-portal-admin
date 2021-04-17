9ed0e762b0b0af0af9c818db253e3dd353de958d
saravj355 <svj971@gmail.com>
2021-04-17 17:27:56 -0500

    add vendor reference and filters

--- src/components/Products/List.js
+++ src/components/Products/List.js
@@ -1,12 +1,42 @@
 import * as React from 'react';
-import { Datagrid, EditButton, List, TextField } from 'react-admin';
+import {
+    Datagrid,
+    EditButton,
+    FunctionField,
+    List,
+    NumberField,
+    ReferenceField,
+    ShowButton,
+    TextField
+} from 'react-admin';
+import ProductFilter from './ProductFilter';
 
-const ListProducts = props => (
-    <List bulkActionButtons={false} {...props}>
+const ListProducts = (props) => (
+    <List
+        filters={<ProductFilter />}
+        {...props}
+        bulkActionButtons={false}
+        sort={{ field: 'isActive', order: 'DESC' }}
+    >
         <Datagrid>
+            <ReferenceField reference="vendors" source="vendorId">
+                <TextField source="name" />
+            </ReferenceField>
             <TextField label="Nombre" source="name" />
-            <TextField label="Precio" source="price" />
-            <EditButton/>
+            <ReferenceField
+                reference="products/categories"
+                source="productCategoryId"
+            >
+                <TextField source="displayName" />
+            </ReferenceField>
+            <NumberField label="Precio" source="price" />
+            <FunctionField
+                label="Estado"
+                sortBy={'isActive'}
+                render={(record) => (record.isActive ? 'ACTIVO' : 'INÁCTIVO')}
+            />
+            <ShowButton />
+            <EditButton />
         </Datagrid>
     </List>
 );
