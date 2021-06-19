ba5bca6184a5c13b1f963ac282dbc42c31ac22fe
saravj355 <svj971@gmail.com>
2021-06-19 14:17:25 -0500

    add entities resource

---
+++ src/components/index.js
@@ -0,0 +1,8 @@
+import Buyers from './Buyers';
+import Categories from './Categories';
+import Dashboard from './Dashboard';
+import Layout from './Layout';
+import Products from './Products';
+import Vendors from './Vendors';
+
+export { Buyers, Categories, Dashboard, Layout, Products, Vendors };
