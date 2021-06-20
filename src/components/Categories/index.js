b84be71a06fea97f3aeecd48d9cc60fac9e16958
saravj355 <svj971@gmail.com>
2021-06-19 20:27:39 -0500

    add category list view

--- src/components/Categories/index.js
+++ src/components/Categories/index.js
@@ -1,9 +1,7 @@
 import BookmarkIcon from '@material-ui/icons/Bookmarks';
 import ListCategories from './ListCategories';
-import ShowCategory from './ShowCategory';
 
 export default {
     list: ListCategories,
-    icon: BookmarkIcon,
-    show: ShowCategory
+    icon: BookmarkIcon
 };
