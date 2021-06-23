b9c7066f6760cd6403bf9528d15b6ee5b752f663
saravj355 <svj971@gmail.com>
2021-06-23 10:11:08 -0500

    add redirect on create

--- src/components/Vendors/CreateVendor.js
+++ src/components/Vendors/CreateVendor.js
@@ -1,10 +1,12 @@
 import * as React from 'react';
 import { Create, SimpleForm, TextInput } from 'react-admin';
 
+const redirectShow = (basePath, id) => `${basePath}/${id}/show`;
+
 const CreateVendor = (props) => {
     return (
         <Create {...props}>
-            <SimpleForm>
+            <SimpleForm redirect={redirectShow}>
                 <TextInput source="name" label="Nombre" />
                 <TextInput source="logoPath" label="Logo" />
                 <TextInput source="contactNumber" label="Número de contacto" />
